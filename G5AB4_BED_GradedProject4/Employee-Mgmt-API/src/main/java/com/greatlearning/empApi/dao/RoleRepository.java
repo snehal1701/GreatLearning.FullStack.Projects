0db7cd226060700e43a6e914842c5a0f1f9f9358
snehal1701 <snehal1701@gmail.com>
2023-02-18 00:58:27 +0530

    Code Cleanup

--- G5AB4_BED_GradedProject4/Employee-Mgmt-API/src/main/java/com/greatlearning/empApi/dao/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.empApi.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.empApi.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
